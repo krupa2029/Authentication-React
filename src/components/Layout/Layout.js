b5846305dcb145d1333f5951ceef9f23d4f2dee8
krupa2029 <kjkotia10@gmail.com>
2021-11-30 21:45:22 +0530

    Add: Main Navigation-Bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import { Fragment } from 'react';
-
+import MainNavigation from './MainNavigation';
 
 const Layout = (props) => {
   return (
     <Fragment>
+      <MainNavigation />
       <main>{props.children}</main>
     </Fragment>
   );
