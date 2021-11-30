4c6121635ff5cc275a49fb6dcb46d6f0a1b59e70
krupa2029 <kjkotia10@gmail.com>
2021-11-30 21:38:07 +0530

    Add: Change-Password Form in Profile-Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { Switch, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Layout from './components/Layout/Layout';
-import AuthPage from './pages/AuthPage';
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Layout from "./components/Layout/Layout";
+import AuthPage from "./pages/AuthPage";
+import UserProfile from './components/Profile/UserProfile';
+
 
 function App() {
   return (
     <Layout>
-    <Switch>
-      <Route path='/' exact>
-        <HomePage />
-      </Route>
-      <Route path='/auth'>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/auth">
           <AuthPage />
         </Route>
-    </Switch>
-  </Layout>
+        <Route path="/profile">
+          <UserProfile />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
--- src/components/Profile/UserProfile.js
+++ src/components/Profile/UserProfile.js
@@ -1,9 +1,11 @@
+import ProfileForm from './ProfileForm';
 import classes from "./UserProfile.module.css";
 
 const UserProfile = () => {
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
+      <ProfileForm />
     </section>
   );
 };
