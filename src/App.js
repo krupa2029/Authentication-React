26dac8b9de57006302f7eafe94e388476085a98f
krupa2029 <kjkotia10@gmail.com>
2021-11-30 21:19:05 +0530

    Add: AuthPage contains Signup and SignIn Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Layout from './components/Layout/Layout';
+import AuthPage from './pages/AuthPage';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <Route path='/' exact>
         <HomePage />
       </Route>
+      <Route path='/auth'>
+          <AuthPage />
+        </Route>
     </Switch>
   </Layout>
   );
