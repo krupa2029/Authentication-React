6486f006b3d36158bf1c5749f1e094ff1116c6f1
krupa2029 <kjkotia10@gmail.com>
2021-11-30 21:30:28 +0530

    Add: Profile Page

---
+++ src/components/Profile/UserProfile.js
@@ -0,0 +1,11 @@
+import classes from "./UserProfile.module.css";
+
+const UserProfile = () => {
+  return (
+    <section className={classes.profile}>
+      <h1>Your User Profile</h1>
+    </section>
+  );
+};
+
+export default UserProfile;
